ee02aca3d349da0719150f157c6bbaaed076bac4
artis33LV <90350400+artis33LV@users.noreply.github.com>
2026-06-11 11:29:30 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'AR Scripts'
+description 'Removes vehicle light corona dots without disabling vehicle lights'
+version '1.0.0'
+
+lua54 'yes'
+
+client_script 'client/main.lua'
